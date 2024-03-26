612921862583f4e95eb09854b4f2813e2784dd8c
Eugene <zaycevea@gmail.com>
2024-03-26 14:14:40 -0400

    Integrate your tests with a Cloud Testing Platform

--- features/environment.py
+++ features/environment.py
@@ -11,14 +11,13 @@ from time import sleep
 from app.application import Application
 
 
-def browser_init(context):
+def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
     # OTHER BROWSERS #
-    # service = FirefoxService("D:/Downloads/geckodriver-v0.34.0-win-aarch64/geckodriver.exe")
-    service = FirefoxService(GeckoDriverManager().install())
-    context.driver = webdriver.Firefox(service=service)
+    # service = FirefoxService(GeckoDriverManager().install())
+    # context.driver = webdriver.Firefox(service=service)
     # context.driver = webdriver.Safari()
 
     # HEADLESS MODE #
@@ -30,21 +29,42 @@ def browser_init(context):
     #     service=service
     # )
 
+    # BROWSERSTACK #
+    # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    bs_user = 'your_user'
+    bs_key = 'your_key'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        # 'os': 'Windows',
+        # 'osVersion': '10',
+        # 'browserName': 'Firefox',
+        # 'sessionName': scenario_name
+
+        'os': 'OS X',
+        'osVersion': 'Big Sur',
+        'browserName': 'Firefox',
+        'sessionName': scenario_name
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
     # HEADED MODE #
     # driver_path = ChromeDriverManager().install()
     # service = Service(driver_path)
     # context.driver = webdriver.Chrome(service=service)
-    context.app = Application(context.driver)
 
     context.driver.maximize_window()
 
     context.driver.implicitly_wait(4)
     context.driver.wait = WebDriverWait(context.driver, 15)
+    context.app = Application(context.driver)
 
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
     context.driver.get('https://soft.reelly.io/sign-in')
     context.driver.find_element(By.ID, 'email-2').send_keys("your_email")
